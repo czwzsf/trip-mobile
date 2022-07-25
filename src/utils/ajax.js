a49633287c3e6b70a58df22841b4623a7cb3289b
zhewei chen <1241770441@qq.com>
2022-07-25 09:19:18 +0800

    跨域

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const ajax = axios.create({
   headers: {
     source: 'h5',
+    icode: 'acbd',
     'Content-Type': 'application/x-www-form-urlencoded'
   },
   withCredentials: true
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// Node.js里面的模块
+module.exports = {
+  // http://localhost:8080/api/test
+  // =>
+  // http://localhost:8000/test
+
+  devServer: {
+    proxy: {
+      '/hello': {
+        // target: 'http://localhost:8000',
+        // 代理的地址
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        pathRewrite: {
+          // '^/api': ''   //需要rewrite重写的URL
+        },
+        '/test': {
+          // target: 'http://localhost:8000',
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/test': ''
+            // 需要rewrite重写的URL
+          }
+        }
+      }
+    }
+  }
+}
