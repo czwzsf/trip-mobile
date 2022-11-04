5895f5142f3f9dc1635770be695afb15cd157450
zhewei chen <1241770441@qq.com>
2022-11-04 09:18:05 +0800

    用户注册（页面开发完成）

--- src/views/accounts/LoginView.vue
+++ src/views/accounts/LoginView.vue
@@ -7,6 +7,29 @@
                  left-arrow
                  @click-left="goBack"/>
     <!--  表单输入-->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        label="用户名"
+        placeholder="用户名"
+        maxlength="11"
+        clearable
+        :rules="ruleName"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
     <!--  文字提示-->
     <div class="tips">
       登入表示同意 用户使用协议及 隐私条款
@@ -26,6 +49,20 @@ import CopyRightVIew from '@/components/common/CopyRightVIew'
 
 export default {
   name: 'LoginView',
+  data () {
+    return {
+      // 用户名的验证规则
+      username: '',
+      password: '',
+      ruleName: [{
+        required: true,
+        message: '请填写用户名'
+      }, {
+        pattern: /1\d{10}/,
+        message: '请填写正确的手机号'
+      }],
+    }
+  },
   components: {
     CopyRightVIew
   },
@@ -33,7 +70,10 @@ export default {
     goBack () {
       // 倒退一级
       this.$router.go(-1)
-    }
+    },
+    onSubmit (values) {
+      console.log('submit', values)
+    },
   },
 }
 </script>
